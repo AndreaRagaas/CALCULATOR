7d2373b9e9b0ae1c5bea0bf1b460c90367cb5d06
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-05-22 17:23:58 +0800

    creating the class

--- user_interface.py
+++ user_interface.py
@@ -1,2 +1,5 @@
 #This User Interface will be the one that accepts the users input. 
 #User Interface will also be the one that will execute the design/graphics of the calculator.
+
+#Creating the class
+class UserInterface:
