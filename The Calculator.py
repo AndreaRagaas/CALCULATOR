cbddd22462981c671c7c90627a8ab0ef5001ced7
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-05-22 18:04:09 +0800

    run the program

--- The Calculator.py
+++ The Calculator.py
@@ -3,3 +3,7 @@
 #Importing the two classes
 from calculator import Calculator
 from user_interface import UserInterface
+
+#Run the program
+if __name__ == "__main__":
+    calculator_gui = UserInterface(Calculator())
