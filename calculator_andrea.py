2a1cea262bcf0671ac29a40aa53ab4008b214f1f
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-06-05 05:06:12 +0800

    import all required modules

--- calculator_andrea.py
+++ calculator_andrea.py
@@ -1,1 +1,11 @@
-#"This will inherit the calculator functionalities, providing a pathway for enhancements and further improvements.
+#This will inherit the calculator functionalities, providing a pathway for enhancements and further improvements.
+
+#Import all required modules
+# Import the calculator
+from calculator import Calculator
+# Import the users interface and ask again
+from user_interface import UserInterface, AskAgain
+# Import the square root operation from math
+from math import sqrt
+# Importing tkinter for the upgraded design of the calculator
+import tkinter as tk
